95f3af9b6688bbc297065921b64086a266a67a4b
VuManh2403 <vdm24032002@gmail.com>
2025-08-12 17:55:50 +0700

    fix

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,18 +1,52 @@
 package com.example.todolist.controller;
 
+import com.example.todolist.entity.TodoEntity;
+import com.example.todolist.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class TodoController {
 
+    private final TodoRepository todoRepository;
+
     @GetMapping({"/", "/home",""})
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        model.addAttribute("todos",todoRepository.findAll());
         return "index";
     }
 
+    @PostMapping("/add")
+    public String add(@RequestParam String title) {
+        TodoEntity todoEntity = TodoEntity.builder()
+                .title(title)
+                .completed(false)
+                .build();
+        todoRepository.save(todoEntity);
+        return "redirect:/";
+    }
 
+    @GetMapping("/update/{id}")
+    public String update (@PathVariable long id) {
+        TodoEntity todoEntity = todoRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Todo not found with id: "+id));
+        todoEntity.setCompleted(true);
+        todoRepository.save(todoEntity);
+        return "redirect:/";
+
+    }
+    @GetMapping("/delete/{id}")
+    public String delete (@PathVariable long id) {
+        TodoEntity todoEntity = todoRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Todo not found with id: "+id));
+        todoRepository.delete(todoEntity);
+        return "redirect:/";
+
+    }
 }
